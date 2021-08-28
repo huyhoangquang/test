f109c3e5f768c3935117a2a1da4ae538a82b3088
hoangquanghuy <btlmo.hoangquanghuy@gmail.com>
2021-08-28 16:28:57 +0700

    firt app ReactJS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Api'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Làm xong project này
+// Làm chuỗi thực hành
+// Sau đó đọc song song với chuỗi lý thuyết các khái niệm cơ bản
+// Sau đó làm project Reacjs trên youtube vừa làm vừa đọc chuỗi lý thuyết cũng cố lại
+// Sau đó viết cv đi xin fresher
+// Sau đó rèn JS và thuật toán JS ... thông qua mozila, javascriptinfo...
+// Học nâng cao Reactjs, các api, các công nghệ liên quan...
+// Nguyên tắc là học với tư duy của bản thân chứ không rập khuôn ..biết mình cần cái nào... chứ không phải đọc hết tài liệu.. no no...như vậy mới thông minh được
+// Cần cái nào, làm cái đó, cái giết chết bản thân mình là sự lo lắng.. ko lo lắng, đang cần học cái nào tự tin học cái đó, ko lo lắng
+// suy diễn nhiều....đơn nhiệm làm từng cái một thôi.
